8c83ea10c457b760389d903793d81d26263f82b3
AcceleratorMX <redfield242@gmail.com>
2023-07-06 18:25:39 +0300

    13th

--- src/main/java/ua/goit/hw13/Main.java
+++ src/main/java/ua/goit/hw13/Main.java
@@ -11,16 +11,16 @@ public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 
-		NoteService noteService = new NoteService();
-
-		noteService.add(new Note(1, "1st title", "First content."));
-		noteService.add(new Note(2, "2nd title", "Second content."));
-		noteService.add(new Note(3, "3rd title", "Third content."));
-
-		List<Note> allNotes = noteService.listAll();
-		for (Note note : allNotes) {
-			System.out.println(note);
-		}
+//		NoteService noteService = new NoteService();
+//
+//		noteService.add(new Note(1, "1st title", "First content."));
+//		noteService.add(new Note(2, "2nd title", "Second content."));
+//		noteService.add(new Note(3, "3rd title", "Third content."));
+//
+//		List<Note> allNotes = noteService.listAll();
+//		for (Note note : allNotes) {
+//			System.out.println(note);
+//		}
 	}
 }
 
--- src/main/java/ua/goit/hw13/Note.java
+++ src/main/java/ua/goit/hw13/Note.java
@@ -2,9 +2,11 @@ package ua.goit.hw13;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Note {
 
     private long id;
